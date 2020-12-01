1182bb28f8cd79bcb69a3acadeac6be20c62cc6a
jerdude <jer157@gmail.com>
2020-12-01 07:47:27 -0500

    Add initial program

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Advent2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day's challenge would you like to run?");
+            
+            string input;
+            
+            do
+            {
+                input = Console.ReadLine();
+                Console.WriteLine("aboo");
+                switch(input)
+                {
+                    case "1":
+                        Console.WriteLine("1");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input.");
+                        break;
+                }
+            }
+            while (input != "q");
+        }
+    }
+}
